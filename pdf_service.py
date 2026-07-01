699acd6e57f327d40ef6cfb7e247b30dffeae3f9
wjhsz666 <w362852792c@gmail.com>
2026-07-01 08:49:59 +0800

    Add files via upload

---
+++ pdf_service.py
@@ -0,0 +1,12 @@
+from pypdf import PdfReader
+
+
+def read_pdf(file):
+    reader = PdfReader(file)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text() or ""
+
+    return text
